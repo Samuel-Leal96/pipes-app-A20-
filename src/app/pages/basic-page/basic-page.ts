67ab6b6d569e9f5db4b00bbcefbd2f47d742ddfc
Samuel-Leal96 <saam_lv96@hotmail.com>
2026-05-19 16:34:58 -0600

    Date Pipes

---
+++ src/app/pages/basic-page/basic-page.ts
@@ -0,0 +1,27 @@
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, effect, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-page',
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
+  templateUrl: './basic-page.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class BasicPage {
+
+  nameLower = signal('samuel');
+  nameUpper = signal('SAMUEL');
+  fullName = signal('SaMueL LeAl VeGA');
+
+  customDate = signal(new Date());
+
+  tickindDateEffect = effect((onCleanup) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+      console.log('tick');
+    }, 1000);
+
+    onCleanup(() => clearInterval(interval));
+  })
+
+}
---
+++ src/app/pages/numbers-page/numbers-page.ts
@@ -0,0 +1,14 @@
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-numbers-page',
+  imports: [DecimalPipe, PercentPipe, CurrencyPipe],
+  templateUrl: './numbers-page.html',
+})
+export default class NumbersPage {
+
+  totalSells = signal(24_233_232.5567);
+  percent = signal(0.4856);
+  currency = signal(1234.56);
+}
