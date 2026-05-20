07533b02b75bf434571ade48bfc1d22c95eb25a7
Samuel-Leal96 <saam_lv96@hotmail.com>
2026-05-20 08:41:48 -0600

    i18SelectPipe (male / female)

--- src/app/pages/uncommon-page/uncommon-page.ts
+++ src/app/pages/uncommon-page/uncommon-page.ts
@@ -1,9 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Card } from "../../components/card/card";
+import { I18nSelectPipe } from '@angular/common';
+
+const client1 = {
+  name: 'Fernando',
+  gender: 'male',
+  age: 40,
+  address: 'Ottawa, Canada'
+}
+
+const client2 = {
+  name: 'Melissa',
+  gender: 'female',
+  age: 30,
+  address: 'Toronto, Canada'
+}
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [Card],
+  imports: [Card, I18nSelectPipe],
   templateUrl: './uncommon-page.html',
 })
-export default class UncommonPage { }
+export default class UncommonPage {
+
+  // i18nSelect
+  client = signal(client1);
+
+  invitationMap = {
+    'male': 'invitarlo',
+    'female': 'invitarla'
+  }
+
+  changeClient(): void {
+    if (this.client() === client1) {
+      this.client.set(client2);
+      return;
+    }
+
+    this.client.set(client1);
+  }
+
+
+}
